25fd89042b070d0fa8fb556c227de660fc197d00
Seth Harrington <Sbgha.09@gmail.com>
2023-01-04 15:43:35 -0600

    Prep 4 ready

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> userNumbers = new List<int>();
+        int nextNumber;
+        do
+        {
+            Console.Write("Enter number: ");
+            nextNumber = int.Parse(Console.ReadLine());
+            if (nextNumber != 0) userNumbers.Add(nextNumber);
+        } while (nextNumber != 0);
+
+        Console.WriteLine($"The sum is: {userNumbers.Sum()}");
+        Console.WriteLine($"The average is: {userNumbers.Average()}");
+        Console.WriteLine($"The largest number is: {userNumbers.Max()}");
+        Console.WriteLine($"The sorted list is:");
+        userNumbers.Sort();
+        foreach (int number in userNumbers) Console.WriteLine(number);
     }
 }

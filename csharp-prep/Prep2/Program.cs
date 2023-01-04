eeb746e194ac17fdefc951246538d365fd815c7d
Seth Harrington <Sbgha.09@gmail.com>
2023-01-04 10:48:43 -0600

    Prep 2 Ready

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter a percentage: ");
+        string valueFromUser = Console.ReadLine();
+        float percentage = float.Parse(valueFromUser);
+        char grade = ' ';
+        char sign = ' ';
+
+        if(percentage >= 90)
+        {
+            grade = 'A';
+        }
+        else if(percentage >= 80)
+        {
+            grade = 'B';
+        }
+        else if(percentage >= 70)
+        {
+            grade = 'C';
+        }
+        else if(percentage >= 60)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'F';
+        }
+        //Sign
+        float remainder = percentage % 10;
+        if(remainder >= 7 && grade != 'A' && grade != 'F')
+        {
+            sign = '+';
+        }
+        else if(remainder <= 3 && grade != 'A' && grade != 'F')
+        {
+            sign = '-';
+        }
+        Console.WriteLine($"Your grade is: {grade}{sign}");
+        if(percentage >= 70)
+        {
+            Console.WriteLine($"Congratulations! You've approved the course.");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you didn't approve the course, you can try again.");
+        }
     }
 }

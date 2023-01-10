dd5b29d89512a8c7e916c249fdac0cf58110dfc0
Seth Harrington <Sbgha.09@gmail.com>
2023-01-10 10:23:07 -0600

    Updating Learning 2 to Display as the sample

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,15 +10,11 @@ class Program
         job1._startYear = 2019;
         job1._endYear = 2022;
 
-        job1.Display();
-
         Job job2 = new Job();
         job2._company="Apple";
         job2._jobTitle = "Operation Engineer";
         job2._startYear = 2015;
         job2._endYear = 2019;
-
-        job2.Display();
 
         Resume resume1 = new Resume();
         resume1._name="Seth Harrington";
@@ -26,9 +22,6 @@ class Program
         resume1._jobs.Add(job1);
         resume1._jobs.Add(job2);
         
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job2._company);
-
         resume1.Display();
     }
 }

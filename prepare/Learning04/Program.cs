ff9101f51d7a400ff1fc880a9c50691070ca2402
Seth Harrington <Sbgha.09@gmail.com>
2023-01-26 23:15:45 -0600

    Learning 04 Ready

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett","Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fraction", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
     }
 }

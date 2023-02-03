571ea936b8d5c5a8d0645c4622660ef98cf20db6
Seth Harrington <Sbgha.09@gmail.com>
2023-02-03 00:22:03 -0600

    Learning 05 Ready

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("Blue", 4);
+        Rectangle rect1 = new Rectangle("Yellow", 2, 8);
+        Circle circle1 = new Circle("Red", 3);
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(square1);
+        shapeList.Add(rect1);
+        shapeList.Add(circle1);
+        foreach (var shape in shapeList)
+        {
+            double area = shape.GetArea();
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {area}.");
+        }
     }
 }
